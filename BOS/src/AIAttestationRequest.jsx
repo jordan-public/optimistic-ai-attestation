e5b56b53e0e0397ae7a78487a67a9631a7b2b0d4
jordan <jordan@bitola.com>
2023-06-24 18:27:31 -0400

    Hide password field

--- BOS/src/AIAttestationRequest.jsx
+++ BOS/src/AIAttestationRequest.jsx
@@ -7,29 +7,24 @@
   //   protocol: "http",
   // });
 
-  async function addFile(content) {
-    // Workaround - return hex string
-    return ethers.utils.sha256(ethers.utils.toUtf8Bytes(JSON.stringify(content)))
+  // async function addFile(content) {
+  //   Once above IPFS library can be imported:
+  //   const { path } = await ipfs.add(content);
+  //   await ipfs.pin.add(path);
+  //   return path;
+  // }
 
-    // Once above IPFS library can be imported:
-    // const { path } = await ipfs.add(content);
-    // await ipfs.pin.add(path);
-    // return path;
-  }
+  // async function getFile(cid) {
+  //   Once above IPFS library can be imported:
+  //   const stream = ipfs.cat(cid);
+  //   let data = "";
 
-  async function getFile(cid) {
-    return "<DUMMY CID RESOLVE>"
+  //   for await (const chunk of stream) {
+  //     data += new TextDecoder().decode(chunk);
+  //   }
 
-    // Once above IPFS library can be imported:
-    // const stream = ipfs.cat(cid);
-    // let data = "";
-
-    // for await (const chunk of stream) {
-    //   data += new TextDecoder().decode(chunk);
-    // }
-
-    // return data;
-  }
+  //   return data;
+  // }
 
   function queryGPT3() {
     const xhr = new XMLHttpRequest();
@@ -56,16 +51,13 @@ console.log("response", response);
 
         attestationRequest = {question: state.question, answer: a };
 
-        addFile(JSON.stringify(attestationRequest))
-          .then((cid) => {
-                console.log("CID: ", cid);
-                State.update({cid: cid});
-            
-                getFile(cid)
-                .then((r) => console.log("Verification: ", JSON.parse(r)))
-                .catch(console.error);
-            })
-          .catch(console.error);
+        State.update({cid: ethers.utils.sha256(ethers.utils.toUtf8Bytes(JSON.stringify(content)))})
+        // addFile(JSON.stringify(attestationRequest))
+        //   .then((cid) => {
+        //         console.log("CID: ", cid);
+        //         State.update({cid: cid});
+        //     })
+        //   .catch(console.error);
       } else {
         console.log("Error: " + xhr.status);
       }
@@ -73,13 +65,13 @@ console.log("response", response);
   }
 
 return (<>
-  <input placeholder="LLM API KEY" onChange={(e) => State.update({apiKey: e.target.value})} />
+  <input type="password" placeholder="LLM API KEY" onChange={(e) => State.update({apiKey: e.target.value})} />
   <textarea
     onChange={(e) => State.update({question: e.target.value})}
     placeholder="Enter your query"
     rows="10"
     cols="50"></textarea>
-  <button onclick="queryGPT3()">Query GPT-3.5</button>
+  <button onclick="queryGPT3">Query GPT-3.5</button>
   <div>{state.answer}</div>
   <div>{state.attestationRequestCID}</div>
 </>)
